5abdaeaa78ef7995b960147f0f8d129a6bc56dab
msauria <mike.sauria@gmail.com>
2017-08-15 12:41:43 -0400

    Updated quasar scripts

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ from distutils.extension import Extension
 
 MAJOR = 1
 MINOR = 4
-PATCH = 0
-ISRELEASED = True
+PATCH = 1
+ISRELEASED = False
 VERSION = '%d.%d' % (MAJOR, MINOR)
 if not PATCH is None:
     VERSION += '.%d' % (PATCH)
